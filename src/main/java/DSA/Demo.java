7fe1522a2d4fe1bcd4fd86813692e4afcc3358db
vinayak Nimbaragi <vinayakakvise@gmail.com>
2026-02-02 21:03:36 +0530

    New Changes

---
+++ src/main/java/DSA/Demo.java
@@ -0,0 +1,9 @@
+package DSA;
+
+public class Demo {
+    public static void main(String[] args) {
+        int n = 10;
+        //febonicSeries(n);
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/Practice/Demo1.java
@@ -0,0 +1,7 @@
+package Practice;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("This is Demo1 class in Practice package.");
+    }
+}
