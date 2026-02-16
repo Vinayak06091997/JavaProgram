7b0bee5c5282de873e0bc83704919e85612b7bed
vinayak Nimbaragi <vinayakakvise@gmail.com>
2026-02-16 23:11:39 +0530

    Java Stream Practice

--- src/main/java/JavaStream/ArrayStream.java
+++ src/main/java/JavaStream/ArrayStream.java
@@ -1,4 +1,148 @@
 package JavaStream;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
 public class ArrayStream {
+
+    public static void main(String[] args){
+        int[] a= {1,2,3,4,5,1};
+        int[] b= {6,7,8,9,0};
+
+        //1. From an int[], filter all even numbers.
+        int[] evenNumber=Arrays.stream(a)
+                                .filter(e->e%2==0)
+                                .toArray();
+        System.out.println("Problem:-1      "+Arrays.toString(evenNumber));
+
+        //2. From an int[], filter all odd numbers.
+        int[] oddNumber=Arrays.stream(a)
+                                .filter(e->e%2!=0)
+                                .toArray();
+        System.out.println("Problem:-2     "+Arrays.toString(oddNumber));
+
+        //3. Find the sum of all elements in an int[].
+        int sum=Arrays.stream(a).reduce(Integer::sum).orElseThrow();
+        System.out.println("Problem:-3     "+sum);
+
+        //4. Find the product of all elements in an int[] using reduce.
+        int productElement=Arrays.stream(a).reduce((a1,b1)->a1*b1).orElseThrow();
+        System.out.println("Problem:-4     "+productElement);
+
+        //5. Find the maximum element in an int[].
+        int maxNumber=Arrays.stream(a).max().orElseThrow();
+        System.out.println("Problem:-5     "+maxNumber);
+
+        //6. Find the minimum element in an int[].
+        int miniNumber=Arrays.stream(a).min().orElseThrow();
+        System.out.println("Problem:-6     "+miniNumber);
+
+        //7. Sort an int[] in ascending order using streams.
+        int[] sortAscending=Arrays.stream(a).sorted().toArray();
+        System.out.println("Problem:-7     "+ Arrays.toString(sortAscending));
+
+        //8. Sort an int[] in descending order using streams.
+        int [] sortDescending=Arrays.stream(a)
+                            .boxed()// convert int to object
+                            .sorted(Comparator.reverseOrder())
+                            .mapToInt(Integer::intValue)  // Object to int
+                            .toArray();
+        System.out.println("Problem:-8     "+ Arrays.toString(sortDescending));
+
+        //9 Remove duplicate elements from an int[].
+        int[] duplicateElement=Arrays.stream(a)
+                                .distinct()
+                                .toArray();
+        System.out.println("Problem:-9     "+ Arrays.toString(duplicateElement));
+
+        //10 Count how many elements are greater than 50.
+        int count=((int) Arrays.stream(a).filter(e -> e > 50).count());
+        System.out.println("Problem:-10     "+ count);
+
+        //11. Find the second highest number in an int[].
+        int secondHighest=Arrays.stream(a)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .orElseThrow();
+        System.out.println("Problem:-11     "+ secondHighest);
+
+        //12 Find the second lowest number in an int[].
+        int secondLowest=Arrays.stream(a)
+                .sorted()
+                .skip(1)
+                .findFirst()
+                .orElseThrow();
+        System.out.println("Problem:-12     "+ secondLowest);
+
+        //13. Find the top 3 highest numbers in an int[].
+        int[] threeHighestNumbers= Arrays.stream(a)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .limit(3)
+                .mapToInt(Integer::intValue)
+                .toArray();
+        System.out.println("Problem:-13     "+ Arrays.toString(threeHighestNumbers));
+
+
+        //14. Find the bottom 3 smallest numbers in an int[].
+        int[] threeLowestNumbers= Arrays.stream(a)
+                .sorted()
+                .limit(3)
+                .toArray();
+        System.out.println("Problem:-14     "+ Arrays.toString(threeLowestNumbers));
+
+
+        //15. Check if any number is greater than 100.
+        boolean anyNumberIsGreaterThan100=Arrays.stream(a)
+                                        .anyMatch(e->e>100);
+
+        System.out.println("Problem:-15     "+ anyNumberIsGreaterThan100);
+
+
+        //16 Check if all numbers are positive.
+        boolean  allNumbersArePositive=Arrays.stream(a)
+                                            .allMatch(e->e>0);
+
+        System.out.println("Problem:-16     "+ allNumbersArePositive);
+
+        //17. Check if none of the numbers are negative.
+        boolean  noneOfTheNumbersAreNegative=Arrays.stream(a)
+                                            .noneMatch(e->e<0);
+        System.out.println("Problem:-17     "+ noneOfTheNumbersAreNegative);
+
+        //18. Skip the first 3 elements and collect the rest into an array.
+        int[] skipFirst3Elements=Arrays.stream(a)
+                                            .skip(3).toArray();
+        System.out.println("Problem:-18     "+ Arrays.toString(skipFirst3Elements));
+
+        //19. Limit the stream to the first 5 elements and collect into an array.
+        int[] limitFirst3Elements=Arrays.stream(a)
+                                            .limit(3).toArray();
+        System.out.println("Problem:-19     "+ Arrays.toString(limitFirst3Elements));
+
+        //20. Find the average of numbers in an int[]
+        int averageNumber= (int) Arrays.stream(a)
+                                    .average()
+                                    .orElseThrow();
+        System.out.println("Problem:-20     "+ averageNumber);
+
+
+
+        //21 sum of 2 array to 3rd array
+
+        int [] c=IntStream.range(0, a.length-1)
+                            .map(i->a[i]+b[i])
+                            .toArray();
+        System.out.println("Problem:-21     "+ Arrays.toString(c));
+
+
+        //22 sum of all elements of both arrays into one number
+        int sumAllElement=IntStream.concat(Arrays.stream(a),Arrays.stream(b))
+                            .sum();
+        System.out.println("Problem:-22     "+ sumAllElement);
+
+    }
 }
--- src/main/java/JavaStream/CharStream.java
+++ src/main/java/JavaStream/CharStream.java
@@ -1,4 +1,107 @@
 package JavaStream;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class CharStream {
+
+    public static void main(String[] args)
+    {
+        String name="vinayak";
+
+
+        //1.Count the frequency of each character in a string.
+        Map<Character,Long> frequency=name.chars()
+                                        .mapToObj(c->(char)c)
+                                        .collect(Collectors.groupingBy(
+                                                                Function.identity(),
+                                                                Collectors.counting()
+                                        ));
+        System.out.println("Problem:-1     "+ frequency);
+
+        //2. Find the first non-repeated character in a string.
+        char nonRepeatedCharacter=name.chars()
+                .mapToObj(c->(char)c)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,     //insertion order
+                        Collectors.counting()
+                ))
+                .entrySet()
+                .stream().filter(e->e.getValue()==1)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElseThrow();
+        System.out.println("Problem:-2     "+ nonRepeatedCharacter);
+
+        //3. Find the first repeated character in a string.
+        char repeatedCharacter=name.chars()
+                .mapToObj(c->(char)c)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,     //insertion order
+                        Collectors.counting()
+                ))
+                .entrySet()
+                .stream().filter(e->e.getValue()>1)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElseThrow();
+        System.out.println("Problem:-3     "+ repeatedCharacter);
+
+        //4.    Reverse a string using streams.
+        String reverseName=name.chars()
+                                    .mapToObj(c->String.valueOf((char)c))
+                                   .reduce("",(a,b)-> (b+a));
+        System.out.println("Problem:-3     "+ reverseName);
+
+
+        //5. Check if a string is a palindrome using streams.
+        boolean isPalindrome= IntStream.range(0,name.length()/2)
+                                .allMatch(i->name.charAt(i)==name.charAt(name.length()-i-1));
+        System.out.println("Problem:-5     " + isPalindrome);
+
+
+        //6. Count the number of vowels in a string.
+        int countVowels= Math.toIntExact(name.chars()
+                                    .mapToObj(c -> (char) c)
+                                    .filter(e -> "aeiouAEIOU".indexOf(e) != -1).count());
+        System.out.println("Problem:-6     " + countVowels);
+
+
+        //7. Remove duplicate characters from a string.
+        String removeDuplicate=name.chars()
+                                .mapToObj(c->String.valueOf((char)c))
+                                .distinct()
+                                .collect(Collectors.joining());
+        System.out.println("Problem:-7     " + removeDuplicate);
+
+        //8. Sort the characters of a string alphabetically.
+        String charSort=name.chars()
+                        .mapToObj(c->String.valueOf((char)c))
+                                .sorted()
+                                .collect(Collectors.joining());
+        System.out.println("Problem:-8     " + charSort);
+
+
+        //9. Find the maximum (highest ASCII) character in a string.
+        char maxChar=name.chars()
+                .mapToObj(c->(char)c)
+                .max(Comparator.naturalOrder())
+                .orElseThrow();
+        System.out.println("Problem:-9     " + maxChar);
+
+        //10. Find the minimum (lowest ASCII) character in a string.
+        char miniChar=name.chars()
+                .mapToObj(c->(char)c)
+                .min(Comparator.naturalOrder())
+                .orElseThrow();
+        System.out.println("Problem:-10    " + miniChar);
+
+    }
 }
